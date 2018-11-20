4dc5c59a2fb431a64d212e14c2a6eb764ca3b537
lilyestrada <lily.cn4@hotmail.com>
2018-11-20 17:06:52 +0100

    Add files via upload

---
+++ test_simulation.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 14:54:28 2018
+
+@author: Lily
+"""
+import os,sys
+import configparser
+import pandas as pd
+import numpy as np
+import collections as col
+from pylab import *
+import errno
+import csv
+from shutil import copy
+from scipy.optimize import fsolve
+import simulation as sim 
+
+
+if __name__ == '__main__':
+    try:
+        INI_file="params.ini" #sys.argv[1]           # e.g "../analysis_scripts/example/params.ini"
+        # First simulation
+        sim.start_transcribing(INI_file, "output")
+        # or you can specify the output path
+        #start_transcribing(INI_file, output_path)
+
+        # Resuming
+        # uncomment this two lines if you want to resume the simulation
+        # 'first_output_path' means the path from which the script will get the npz files
+        #first_output_path=sys.argv[2]        # e.g "../analysis_scripts/example/first_output"
+        #start_transcribing(INI_file, first_output_path, resume=True)
+    except configparser.NoSectionError:
+        print("Error ! Please check the path to the paraneters files !")
+        sys.exit(1)
+    except PermissionError:
+        print("Permission denied ! Please check the directory to the output files !")
+        sys.exit(1)
+    except (FileNotFoundError, NameError):
+        print("Error ! Please check the directory to the output files !")
+        sys.exit(1)
