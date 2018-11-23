9cf252d3674f79adb8c9c4924784cc13025594d2
Yueyuxiao Yang <594912907@qq.com>
2018-11-23 10:01:58 +0100

    Update test_simulation.py

--- test_simulation.py
+++ test_simulation.py
@@ -5,19 +5,42 @@ Created on Tue Nov 20 14:54:28 2018
 @author: Lily
 """
 
-#sys.path.append('/home/pbazelle/Documents/5BIM/biologie computationnelle/Biologie-computationnelle/SamMayer/TCDS-v2-master/TCDS/') 
-import simulation as sim 
-import os,sys
-import configparser
-import pandas as pd
+import sys
+import matplotlib
 import numpy as np
-import collections as col
-from pylab import *
-import errno
-import csv
-from shutil import copy
-from scipy.optimize import fsolve
+#sys.path.append('/home/yyang1/Bureau/Biologie-computationnelle/') 
+import simulation as sim 
+#import SamMayer.TCDS_v2.TCDS.plotting_v2 as plotting
+#matplotlib.rcParams.update({'font.size': 13})
 
+INI_file= 'params.ini'
+output_dir='output'
+
+output_dir_res = output_dir+"/all_res"
+sim.start_transcribing(INI_file, output_dir)
+
+'''
+try:
+    #output_dir=sys.argv[2]
+    
+except:
+    print("sim goes wrong")
+'''
+'''
+sigma_info = np.load(output_dir_res+"/save_sigma_info.npz")
+RNAPs_info = np.load(output_dir_res+"/save_RNAPs_info.npz")
+
+Barr_sigma_info = sigma_info["Barr_sigma_info"]
+Dom_size_info = sigma_info["Dom_size_info"]
+
+for i, Barr_sigma_val in enumerate(sigma_info["Barr_sigma_info"]):
+	one_sigma_info = np.repeat(Barr_sigma_val, sigma_info["Dom_size_info"][i])
+	RNAPs_pos_info = RNAPs_info["RNAPs_info"][:, 1, i]
+	plotting.plot_mean_sigma_genes_v2(INI_file, one_sigma_info, RNAPs_pos_info)
+'''
+
+
+'''
 if __name__ == '__main__':
     try:
         INI_file="params.ini" #sys.argv[1]           # e.g "../analysis_scripts/example/params.ini"
@@ -40,3 +63,4 @@ if __name__ == '__main__':
     except (FileNotFoundError, NameError):
         print("Error ! Please check the directory to the output files !")
         sys.exit(1)
+'''
