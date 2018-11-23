45ab6d7e88d6ab9f39bd60f440ba05962ce658d0
Yang Yueyuxiao <yyang1@bs406-s33-10.insa-lyon.fr>
2018-11-23 08:34:28 +0100

    delete old version

--- SamMayer/TCDS-v2-master/analysis_scripts/start_simulation.py
+++
@@ -1,8 +0,0 @@
-import sys
-import TCDS.simulation as sim
-INI_file=sys.argv[1]
-try:
-    output_dir=sys.argv[2]
-    sim.start_transcribing(INI_file, output_dir)
-except:
-    sim.start_transcribing(INI_file)
